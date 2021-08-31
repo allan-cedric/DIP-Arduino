1888ab00ac7a7c9f8ccfbb3bfd7955d744fb6c6c
Allan Cedric <allan.py3000@gmail.com>
2021-08-31 15:48:57 -0300

    docs: functions and instructions

--- DIP-Arduino.cpp
+++ DIP-Arduino.cpp
@@ -1,3 +1,7 @@
+// -- Implementação da classe DIPSwitch --
+// -- Plataforma: Arduino --
+// -- Autor: Allan Cedric --
+
 #include "DIP-Arduino.h"
 
 DIPSwitch::DIPSwitch(uint8_t *pins, uint8_t nPins)
@@ -5,7 +9,7 @@ DIPSwitch::DIPSwitch(uint8_t *pins, uint8_t nPins)
     _pins = pins;
     _nPins = nPins;
     for (uint8_t i = 0; i < _nPins; i++)
-        pinMode(_pins[i], STD_INPUT);
+        pinMode(_pins[i], STD_INPUT); // Seta cada pino do DIP como STD_INPUT
 }
 
 uint8_t DIPSwitch::getState()
@@ -14,7 +18,7 @@ uint8_t DIPSwitch::getState()
     for (uint8_t i = 0; i < _nPins; i++)
     {
         if (!digitalRead(_pins[i]))
-            state |= (1 << (_nPins - i - 1));
+            state |= (1 << (_nPins - i - 1)); // Seta bit 1 na posição (_nPins - i - 1)
     }
     return state;
 }
--- DIP-Arduino.h
+++ DIP-Arduino.h
@@ -1,22 +1,37 @@
+// -- Classe que molda o comportamento de um DIP Switch --
+// -- Plataforma: Arduino --
+// -- Autor: Allan Cedric --
+
 #ifndef __DIP_ARDUINO_H__
 #define __DIP_ARDUINO_H__
 
 // --- Bibliotecas ---
 #include <Arduino.h>
 
+// --- Macros ---
 #define STD_INPUT INPUT_PULLUP
 
 // --- Classe DIPSwitch ---
 class DIPSwitch
 {
 private:
-    uint8_t *_pins;
-    uint8_t _nPins;
+    uint8_t *_pins; // Vetor de pinos do DIP
+    uint8_t _nPins; // Número de pinos do DIP
+
 public:
-    // Construtor
+    /*!
+        @brief  Construtor
+
+        @param  pins    Ponteiro para o vetor de pinos (Deve ser alocado previamente)
+        @param  nPins   Tamanho do vetor de pinos
+    */
     DIPSwitch(uint8_t *pins, uint8_t nPins);
 
-    // Retorna o estado em binário do DIPSwitch
+    /*!
+        @brief  Retorna o estado em binário do DIP
+
+        @return Estado do DIP
+    */
     uint8_t getState();
 };
 
